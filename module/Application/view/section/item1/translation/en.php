0fdc46a44dd53737193277e4345dfd4c0a2b4227
alex <alexej.kisselev@gmail.com>
2022-12-05 16:14:23 +0100

    gallery item ready

--- module/Application/view/section/item1/translation/en.php
+++ module/Application/view/section/item1/translation/en.php
@@ -6,9 +6,9 @@
  * author: alexej@kisselev.de
  */
 return [
-    'post.title' => 'Permanent lip make-up',
-    'post.topic' => 'Lip care after permanent make-up',
-    'post.date' => '27 February 2022',
-    'post.text1' => 'Lorem ipsum dolor sit amet, consectet ad elit sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci. tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum ',
-    'post.text2' => ''
+    'item.title' => 'Permanent lip make-up',
+    'item.topic' => 'Lip care after permanent make-up',
+    'item.date'  => '27 February 2022',
+    'item.text1' => 'Lorem ipsum dolor sit amet, consectet ad elit sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci. tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum ',
+    'item.text2' => ''
 ];
---
+++ module/Application/view/section/item2/translation/ru.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * File ru.php
+ *
+ * since: 27.11.2022
+ * author: alexej@kisselev.de
+ */
+return [
+    'item.title' => 'Перманентный макияж губ',
+    'item.topic' => 'Уход за губами после перманентного макияжа',
+    'item.date'  => '27 февраля 2022',
+    'item.text1' => 'Lorem ipsum dolor sit amet, consectet ad elit sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci. tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum ',
+    'item.text2' => 'Lorem ipsum dolor sit amet, consectet ad elit sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci. tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum '
+];
