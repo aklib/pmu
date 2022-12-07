d818f478d279e5eeff5799b8b898a5c1ac4fb636
alex <alexej.kisselev@gmail.com>
2022-12-07 15:41:10 +0100

    prices ready little changes

--- module/Application/view/section/prices/translation/ru.php
+++ module/Application/view/section/prices/translation/ru.php
@@ -6,10 +6,10 @@
  * author: alexej@kisselev.de
  */
 return [
-    'prices.title'      => 'Цены',
-    'price.eye.name1'   => 'Перманентный макияж бровей',
-    'price.eye.amount1' => '120 000 руб.',
+    'prices.title'     => 'Цены',
+    'price.eye.name1'  => 'Перманентный макияж бровей',
+    'price.eye.price1' => '120 000 руб.',
 
-    'price.eye.name2'   => 'Перекрытие старого татужа',
-    'price.eye.amount2' => '100 000 руб.',
+    'price.eye.name2'  => 'Перекрытие старого татужа',
+    'price.eye.price2' => '100 000 руб.',
 ];
